768c4fae068aed7c877fb01844c5298912023e81
TheKingsident <kingsident@gmail.com>
2024-04-11 21:15:07 +0100

    Updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,11 @@
 /* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
-    this._sqft = this._validateSQFT(sqft, 'sqft');
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  _validateSQFT(value, property) {
-    if (typeof value !== 'number' || Number.isNaN(value)) {
-      throw new TypeError(`${property} must be a number`);
-    }
-    return value;
   }
 
   evacuationWarningMessage() {
