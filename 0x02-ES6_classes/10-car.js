f76e102916cf5994fafbe780e33ae53b20ac468b
TheKingsident <kingsident@gmail.com>
2024-04-11 22:42:17 +0100

    Added files for tasks 8 to 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,29 @@
+const cloneCarSymbol = Symbol('cloneCar');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  [cloneCarSymbol]() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneCarSymbol]();
+  }
+}
