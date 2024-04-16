100d1da1ecc13a9088583bb2162412781c409a25
TheKingsident <kingsident@gmail.com>
2024-04-16 21:27:51 +0100

    Updated

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8TypedArray = new Int8Array(buffer);
-
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  int8TypedArray[position] = value;
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  return buffer;
+  return view;
 }
