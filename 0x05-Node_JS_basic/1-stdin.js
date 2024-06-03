3878bc8e747410b13fdcadd36fe889658575a5d8
TheKingsident <kingsident@gmail.com>
2024-06-03 21:09:44 +0100

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdin.setEncoding('utf-8');
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
+console.log('Welcome to Holberton School, what is your name?');
 
-process.on('exit', () => {
+process.stdin.on('data', (data) => {
+  const userName = data.trim();
+  console.log(`Your name is: ${userName}`);
   console.log('This important software is now closing');
+  process.exit();
 });
