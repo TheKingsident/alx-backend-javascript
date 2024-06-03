54c1f9487d5f53b7c9ade6e9da05d63d5db9c0bf
TheKingsident <kingsident@gmail.com>
2024-06-03 21:15:19 +0100

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,6 +5,13 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const userName = data.trim();
   console.log(`Your name is: ${userName}`);
-  console.log('This important software is now closing');
-  process.exit();
 });
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
+// This is necessary to keep the process running if using a pipe
+if (process.stdin.isTTY) {
+  process.stdin.resume();
+}
